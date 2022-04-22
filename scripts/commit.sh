bda48e0957ea649a3c3d5bc74cfd713b0f1cd3e4
Felix Zailskas <33928721+felix-zailskas@users.noreply.github.com>
2022-04-22 13:08:32 +0200

    Update commit.sh

--- scripts/commit.sh
+++ scripts/commit.sh
@@ -1,29 +1,25 @@
 #!/bin/bash
+# adds all current files to git, creates a commit with message given as 
+# command line arguments or as input then pushes to the current branch if wanted
 git add -A
 
-# this reads all the command line arguments and 
-# merges them into one string
-cmt_msg="$*"
+# this reads all the command line arguments and megerges them into one string
+commit_msg="$*"
 
-# if the length of the commit message is 0 then we 
-# have to request a commit message from the user
-if [ ${#cmt_msg} = 0 ]
-then
+if [ ${#commit_msg} = 0 ]
+then 
     echo Enter commit message:
-    read cmt_msg
+    read commit_msg
 fi
 
-git commit -m "$cmt_msg"
+git commit -m "$commit_msg"
 
-# ask the user if they want to push the commit
-echo Do you want to push this commit? [y / n]
+# ask user if they want to push the commit 
+branch=$(git rev-parse --abbrev-ref HEAD)
+echo Do you want to push this commit to $branch ? [y / n]
 read push_wish
-echo $push_wish
-# only push if answer is y all other inputs are considered as n
 if [ $push_wish = "y" ]
 then
-    branch=$(git rev-parse --abbrev-ref HEAD)
-    echo pushing to $branch...
     git push --set-upstream origin $branch
 else
     echo not pushing to github
